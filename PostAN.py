a13917cc162307fd53a9e836045d369ff1d720ec
bisnupriyasahu <49670527+bisnupriyasahu@users.noreply.github.com>
2022-02-07 23:12:12 +0530

    Added MT2

--- PostAN.py
+++ PostAN.py
@@ -8,7 +8,7 @@ import math
 import re
 from ROOT import *
 import numpy as np
-
+from mt2 import mt2
 
 try:
   input = raw_input
@@ -77,13 +77,15 @@ branchFatJet          = treeReader.UseBranch('JetPUPPIAK8')
 outputfile = ROOT.TFile(outputFile, 'RECREATE')
 tauPT_1 = ROOT.TH1F("tau1_pt", "tau P_{T}", 100, 0.0, 1000.0)
 tauPT_2 = ROOT.TH1F("tau2_pt", "tau P_{T}", 100, 0.0, 1000.0)
-metPT = ROOT.TH1F("met_pt", "met P_{T}", 100, 0.0, 1000.0)
-HT_Tot = ROOT.TH1F("HT", "P_{T}", 100, 0.0, 1000.0)
-ptratio_tau1 = ROOT.TH1F("ptratio_tau1", "P_{T}", 50, 0.0, 2.0)
-ptratio_tau2 = ROOT.TH1F("ptratio_tau2", "P_{T}", 50, 0.0, 2.0)
-gen_ptratio_tau1 = ROOT.TH1F("gen_ptratio_tau1", "P_{T}", 50, 0.0, 2.0)
-gen_ptratio_tau2 = ROOT.TH1F("gen_ptratio_tau2", "P_{T}", 50, 0.0, 2.0)
-
+metPT = ROOT.TH1F("met_pt", "MET", 100, 0.0, 1000.0)
+HT_Tot = ROOT.TH1F("HT", "Sum P_{T}", 100, 0.0, 1000.0)
+ptratio_tau1 = ROOT.TH1F("ptratio_tau1", "P_{T} Ratio", 50, 0.0, 2.0)
+ptratio_tau2 = ROOT.TH1F("ptratio_tau2", "P_{T} Ratio", 50, 0.0, 2.0)
+genmatch_ptratio_tau1 = ROOT.TH1F("genmatch_ptratio_tau1", "P_{T} Ratio", 50, 0.0, 2.0)
+genmatch_ptratio_tau2 = ROOT.TH1F("genmatch_ptratio_tau2", "P_{T} Ratio", 50, 0.0, 2.0)
+notgenmatch_ptratio_tau1 = ROOT.TH1F("notgenmatch_ptratio_tau1", "P_{T} Ratio", 50, 0.0, 2.0)
+notgenmatch_ptratio_tau2 = ROOT.TH1F("notgenmatch_ptratio_tau2", "P_{T} Ratio", 50, 0.0, 2.0)
+MT = ROOT.TH1F("MT", "MT2", 100, 0.0, 1000.0)
 
 
 
@@ -105,26 +107,15 @@ for entry in range(0, numberOfEntries):
   i = 0
 
   for iTau1, tau1 in enumerate(branchJet) :
-    
     i +=1
     tautagOk1 = ( tau1.TauTag & (1 << 2) )
-    if (tau1.PT >30 and abs(tau1.Eta) < 3. and tautagOk1):
-      if (abs(tau1.Charge) != 1): continue
-      #else: print "tau1.Charge", tau1.Charge
-      for iTau2, tau2 in enumerate(branchJet) :
-        if (iTau1 == iTau2) : continue
-        tautagOk2 = ( tau2.TauTag & (1 << 2) )
-        if (tau2.PT >30 and abs(tau2.Eta) < 3. and tautagOk2):
-          if (abs(tau2.Charge) != 1):
-            continue
-          #else: print "tau2.Charge", tau2.Charge
+    if (not (tau1.PT >30 and abs(tau1.Eta) < 3. and tautagOk1 and abs(tau1.Charge) == 1)): continue
+    for iTau2, tau2 in enumerate(branchJet):
+      if (iTau1 == iTau2) : continue
+      tautagOk2 = ( tau2.TauTag & (1 << 2) )
+      if (not (tau2.PT >30 and abs(tau2.Eta) < 3. and tautagOk2 and abs(tau2.Charge) == 1)): continue
       if (tau1.Charge*tau2.Charge < 0):
-          
-        
         HT = tau1.PT + tau2.PT
-        Tltau1_p4.SetPtEtaPhiM(tau1.PT, tau1.Eta, tau1.Phi, tau1.Mass)
-        Tltau2_p4.SetPtEtaPhiM(tau2.PT, tau2.Eta, tau2.Phi, tau2.Mass)
-        #print "HT in loop ", HT
         if (HT > tau1_tau2_HT) :
           tau1_idx = iTau1
           tau2_idx = iTau2
@@ -157,8 +148,10 @@ for entry in range(0, numberOfEntries):
     
   Met_PT = -1
   imet_idx = -1
+  Met_Phi = 0
   for imet, met in enumerate(branchPuppiMissingET):
     Met_PT = met.MET
+    Met_Phi = met.Phi
     imet_idx = imet
   #print imet_idx
  
@@ -200,7 +193,7 @@ for entry in range(0, numberOfEntries):
 
   isLeptonic2 = False  
   tau2_leadCH = None
-  for consti2 in tau1.Constituents:
+  for consti2 in tau2.Constituents:
     #print("coming inside 2")
     ids2 = consti2.PID
     if (abs(ids2) in [11, 12, 13, 14]):
@@ -212,21 +205,23 @@ for entry in range(0, numberOfEntries):
     dR2 = const2_p4.DeltaR(Tltau2_p4)
     
     if (dR2 < 0.1):
-      print("coming inside 3 dr2 :",dR2)  
+      #print("coming inside 3 dr2 :",dR2)  
       chpt = consti.PT
       if (tau2_leadCH is None or consti2.PT > tau2_leadCH.PT):
         tau2_leadCH = consti2
   if (tau2_leadCH is not None):
     leadchtau2 =  tau2_leadCH.PT/tau2.PT
-    print("leadchtau2 is ",leadchtau2)
+    #print("leadchtau2 is ",leadchtau2)
     ptratio_tau2.Fill(leadchtau2)
   
 
  
   gen_1 = None
   gen_2 = None
-  print("Branch part : ", branchParticle.GetEntries(), entry)
-  print( "Branch of jets :", branchJet.GetEntries(), entry)
+  #print("Branch part : ", branchParticle.GetEntries(), entry)
+  #print( "Branch of jets :", branchJet.GetEntries(), entry)
+  leadchtau1 = -1
+  leadchtau2 = -1
   for igen,gen in enumerate(branchParticle):
     if (abs(gen.PID) == 15):
       #print "gen pid ",gen.PID
@@ -242,13 +237,43 @@ for entry in range(0, numberOfEntries):
         #print "gen2 pt ", gen.PT          
   
   if (gen_1 is not None):
+    #print (leadchtau1)
     gen_1pt =  gen_1.PT/tau1.PT
-    gen_ptratio_tau1.Fill(gen_1pt)
+    genmatch_ptratio_tau1.Fill(leadchtau1)
+  else: 
+    print (leadchtau1)
+    notgenmatch_ptratio_tau1.Fill(leadchtau1)
   if (gen_2 is not None):
     gen_2pt =  gen_2.PT/tau2.PT
-    gen_ptratio_tau2.Fill(gen_2pt)
-
+    genmatch_ptratio_tau2.Fill(leadchtau2)
+  else:
+    print (leadchtau2)
+    notgenmatch_ptratio_tau2.Fill(leadchtau2)
     
+
+  tau1_px = Tltau1_p4.Px()
+  tau1_py = Tltau1_p4.Py()
+  tau1_m = tau_1.Mass
+  #print("tau1_px and tau1_py", tau1_px, tau1_py)
+
+  tau2_px = Tltau2_p4.Px()
+  tau2_py = Tltau2_p4.Py()
+  tau2_m = tau_2.Mass
+  #print("tau2_px and tau2_py", tau2_px, tau2_py)
+
+  MET_px = Met_PT*(math.cos(Met_Phi))
+  MET_py = Met_PT*(math.sin(Met_Phi))
+  #print("Met_px and MET_py", MET_px, MET_py)
+
+
+  MT_ = mt2(
+    tau1_m,tau1_px,tau1_py,
+    tau2_m,tau2_px,tau2_py,
+    MET_px,MET_py,
+    0,0)
+  print("MT is : ", MT_)
+  MT.Fill(MT_)
+
 
 #cnv.cd(2)
 outputfile.cd()
@@ -258,8 +283,11 @@ metPT.Write()
 HT_Tot.Write()
 ptratio_tau1.Write()
 ptratio_tau2.Write()
-gen_ptratio_tau1.Write()
-gen_ptratio_tau2.Write()
+genmatch_ptratio_tau1.Write()
+genmatch_ptratio_tau2.Write()
+notgenmatch_ptratio_tau1.Write()
+notgenmatch_ptratio_tau2.Write()
+MT.Write()
 
 print( tauPT_1.GetEntries())
 print( tauPT_2.GetEntries())
@@ -267,8 +295,10 @@ print( metPT.GetEntries())
 print( HT_Tot.GetEntries())
 print( ptratio_tau1.GetEntries())
 print( ptratio_tau2.GetEntries())
-print( gen_ptratio_tau1.GetEntries())
-print( gen_ptratio_tau2.GetEntries())
-
+print( genmatch_ptratio_tau1.GetEntries())
+print( genmatch_ptratio_tau2.GetEntries())
+print( notgenmatch_ptratio_tau1.GetEntries())
+print( notgenmatch_ptratio_tau2.GetEntries())
+print( MT.GetEntries())
 outputfile.Close()
 input("Press Enter to continue...")
