86f4d140aaf896eac3714ea8ca2587dbeef0669d
bisnupriyasahu <49670527+bisnupriyasahu@users.noreply.github.com>
2022-02-09 15:26:36 +0530

    updated

---
+++ submit_updated.sh
@@ -0,0 +1,15 @@
+#./MakeCondorFiles.sh PostAN.py root://eoscms.cern.ch///store/group/upgrade/Snowmass2021/Delphes/TT_TuneCUETP8M2T4_14TeV-powheg-pythia8_200PU/TT_TuneCUETP8M2T4_14TeV-powheg-pythia8_8*.root TTbar_0_.root TTbar_0
+
+./MakeCondorFiles.sh PostAN.py root://eoscms.cern.ch///store/group/upgrade/Snowmass2021/Delphes/TT_TuneCUETP8M2T4_14TeV-powheg-pythia8_200PU/TT_TuneCUETP8M2T4_14TeV-powheg-pythia8_8*.root TTbar_0_.root TTbar_0 classes/DelphesClasses.h external/ExRootAnalysis/ExRootTreeReader.h 
+
+#./MakeCondorFiles.sh PostAN.py root://eoscms.cern.ch///store/group/upgrade/Snowmass2021/Delphes/TT_TuneCUETP8M2T4_14TeV-powheg-pythia8_200PU/TT_TuneCUETP8M2T4_14TeV-powheg-pythia8_8*.root TTbar_8_.root TTbar_8 /nfs_scratch/bsahu/Snowmass_2022/Post_Analyzr/CMSSW_12_2_0/delphes/
+
+
+
+
+
+
+
+
+
+
